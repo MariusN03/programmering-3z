7237a59df15533261ecb1c7f0eaa6fa7f741d25d
MariusN03 <mari71k1@edu.nextkbh.dk>
2022-09-20 12:51:03 +0200

    meh

--- nodejs-express-p5-template/index.js
+++ nodejs-express-p5-template/index.js
@@ -1,21 +1,49 @@
 const ip = require('ip')
 console.log(ip.address())
 
+const socketLib = require('socket.io')
+
+let colors = ['red', 'blue', 'purple', 'green', 'brown', 'pink', 'yellow', 'cyan']
+
 const express = require('express')
 
 const app = express()
 
+//json array til brugere
+let users = []
 
 const port = 666
+
+const server = app.listen(port, ()=>{
+    console.log('server lytter på port ' + port)
+})
+
+const serverSocket = socketLib(server)
 
 app.use('/', express.static('public/'))
 
 app.get('/ip', (req, res)=>{
-res.json(ip.address())
-console.log('yoooo')
+    res.json(
+        {
+            'ip': ip.address(),
+            'port': port
+        }
+    )
+    console.log('yoooo')
 
 })
 
-app.listen(port, ()=>{
-    console.log('server lytter på port ' + port)
+serverSocket.sockets.on('connection', socket => {
+    console.log('new socket established, super cool')
+    //socket.on er en eventlistener på nye beskeder fra klienter
+    socket.on('chat', message => {
+        console.log(message)
+        //når serveren modtager besked så sender den dem rundt til alle via newMessage
+        serverSocket.sockets.emit('newMessage', message)        
+    })
+
+    socket.on('newUser', user =>{
+        users.push({'name': user,'id':socket.id})
+        console.log(users)
+    })
 })
--- nodejs-express-p5-template/public/index.js
+++ nodejs-express-p5-template/public/index.js
@@ -1,15 +1,65 @@
 let bg = 'lightblue'
+let clientSocket
+let state = 'enterName'
+let userName
+
 function setup(){
     createCanvas(windowWidth, windowHeight)
-    
+  
+
     background(bg)
 
     //fetch server ip from node endpoint
-    fetch('http://localhost/666/ip')
+    fetch('http://localhost:666/ip')
         .then(res => res.json())
-        .then(json => select('#info').html(json))
+        .then(json => {
+            select('#info').html(json.ip + ':' + json.port)
+
+    })
+    //io kommer fra socket.io library
+    clientSocket = io.connect()
+    
+    
+    select('#nameButton').mousePressed(()=>{
+        console.log('ny burger')
+        clientSocket.emit('newUser', select('#name').value())
+        select('#nameBox').addClass('hide')
+        select('#chatBox').removeClass('hide')
+        userName = select('#name').value()
+    })
+
+    clientSocket.on('newMessage', message => {
+        let p
+        if(message.userName ==userName ){
+            p = createElement('p', message.message)
+            p.addClass('me')
+        }else{
+            p = createElement('p', message.userName + ': ' + message.message)
+        }
+        
+        select('#chat').child(p)
+        select('#chat').elt.scrollTop = select('#chat').elt.scrollHeight
+    })
+    
 }
+
 
 function draw(){
     
+}
+
+function keyPressed(key){
+    let message = select('#message').value()
+    if(key.key=='Enter'&& message != ''){
+        
+        console.log(message)
+        select('#message').value('')
+        clientSocket.emit('chat', {'userName':userName, 'message': message})
+    }
+}
+
+function mousePressed(){
+    let green = map(mouseX,0,windowWidth, 0 ,255)
+    fill(0, green, 0)
+    //ellipse(mouseX, mouseY, 20)
 }
