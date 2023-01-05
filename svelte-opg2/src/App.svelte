2f96a69728fdad3545b1389945ed8a86aa1e96f0
MariusN03 <mari71k1@edu.nextkbh.dk>
2023-01-05 22:34:14 +0100

    planter?

--- svelte-opg2/src/App.svelte
+++ svelte-opg2/src/App.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <main>
-	<div>	
+	<div class="shits">	
 		<h2>Navn:</h2>
 		<input type="text" bind:value={name}>
 		<h1 id="kent">Hello {name}!</h1>
@@ -17,7 +17,7 @@
 		<button on:click={()=>{text = val; val = ''}}>OK!</button>
 	</div>
 
-	<div>	
+	<div class="shits">	
 		<button id="Dkent" on:click={()=>discoKent = !discoKent}>{name} disco!</button>
 	</div>
 
@@ -31,20 +31,28 @@
 
 <style>
 	:global(body){
-		background-color: black;
+		background-image: url("https://png.pngtree.com/background/20210715/original/pngtree-plant-palm-leaf-simple-background-picture-image_1325702.jpg");
 		overflow: hidden;
-		
+		background-size: cover;
+		padding: 0;
+		margin: 0;
 	}
-
+/*
+https://media.tenor.com/_4YgA77ExHEAAAAd/rick-roll.gif
+*/
 	#partyparty {
 		background-image: url("https://i.pinimg.com/originals/bc/52/ac/bc52ac9b9a742c4e255977e191461319.gif");
 		background-size: cover;
 		position: fixed;
-		z-index: -1;
+		z-index: 1;
 		top: 0;
 		left: 0;
 		width: 100vw;
 		height: 100vh;
+	}
+
+	.shits {
+		z-index: 5;
 	}
 
 	main {
@@ -56,6 +64,13 @@
 		display: grid;
 		grid-template-rows: 1fr 1fr;
 		gap: 10vh;
+		background: rgb(0, 32, 0);
+		background: linear-gradient(140deg, rgb(4, 78, 0) 0%, rgb(14, 48, 0) 50%, rgba(4, 78, 0) 100%);
+		width: 60vw;
+		height: 100vh;
+		border-top-left-radius: 25rem;
+		border-bottom-right-radius: 25rem;
+		z-index: 0;
 	}
 
 	#Dkent:hover {
