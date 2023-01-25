0e02b320889135776024b395d52ac3494e82aa18
MariusN03 <mari71k1@edu.nextkbh.dk>
2023-01-25 09:08:34 +0100

    opg 4 gems

--- svelte-opg4/src/App.svelte
+++ svelte-opg4/src/App.svelte
@@ -1,36 +1,51 @@
 <script>
 	let counter = 0
-	let juiceImage
-	let juices = ['orange', 'apple', 'banana', 'ginger', 'tropical']
-	$: if(counter >= juices.length) counter = 0
-	$: fetch("https://api.giphy.com/v1/gifs/search?api_key=u4QsbdJ55v6W3irNyXGgT0Hg3O6W7jSK&limit=1&q=" + juices[counter])
+	let stoneImage
+	let stones = ['diamond', 'ruby gem', 'emerald', 'sapphire', 'amethyst stone']
+	$: if(counter >= stones.length) counter = 0
+	$: fetch("https://api.giphy.com/v1/gifs/search?api_key=u4QsbdJ55v6W3irNyXGgT0Hg3O6W7jSK&limit=1&q=" + stones[counter])
 		.then(res => res.json())
 		.then(json => {
 			console.log(json.data)
-			juiceImage = json.data[0].images.original.url
-			console.log(juiceImage)
+			stoneImage = json.data[0].images.original.url
+			console.log(stoneImage)
 			})
 </script>
 
 <main>
-	<img class='juiceIMG' src="{juiceImage}" alt="">
-	<button on:click={()=>{
-		counter = counter + 1
-		}}>Knappa</button>
+	<div class="box">
+		<img id='juiceIMG' src="{stoneImage}" alt="">
+	<br>
+		<button on:click={()=>{
+			counter = counter + 1
+			}}>$$$</button>
+
+	</div>
 	
 </main>
 
 <style>
 
+	:global(body){
+		margin: 0;
+		padding: 0;
+	}
+
 main{
+	height: 100vh;
+	display: grid;
+	place-items: center;
+	background-color: black;
+}
+
+.box{
 	display: grid;
 	place-items: center;
 }
 
-.juiceIMG{
+#juiceIMG{
     width:10rem;
     height:10rem;
     object-fit: cover;
-    border-radius: 50%;
 }
 </style>
