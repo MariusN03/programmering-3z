b1d6bedc3e3abf7ec6f9a2154eabe3195bcfc796
MariusN03 <mari71k1@edu.nextkbh.dk>
2023-02-01 11:15:28 +0100

    template

---
+++ sveltejs-template/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<main>
+	<h1>Hello svelte!</h1>
+</main>
+
+<style>
+:global(*, body){
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+}
+
+	main {
+		display: grid;
+		place-items: center;
+	}
+
+</style>
